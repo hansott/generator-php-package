154584eb460e98b6322a03a4cc34dcf00f951ad2
Hans Ott <hansott@hotmail.be>
2016-05-16 15:17:29 +0200

    Add first working version

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,81 +1,102 @@
 'use strict';
-var yeoman = require('yeoman-generator');
+var path = require('path');
 var chalk = require('chalk');
 var yosay = require('yosay');
-var path = require('path');
 var username = require('username');
-var fullname = require('fullname');
+var yeoman = require('yeoman-generator');
+var transform = require('gulp-transform');
 var isEmailLike = require('is-email-like');
+var shell = require('shelljs');
+
+var skeletonVersion = '37f9078c76b785205786c2ebb976a077787c98c8';
+var skeletonShortVersion = skeletonVersion.slice(0, 7);
+var skeletonOrganisation = 'thephpleague';
+var skeletonRepository = 'skeleton';
+var skeletonTarballUri = 'https://github.com/' + skeletonOrganisation + '/' + skeletonRepository + '/tarball/' + skeletonVersion;
+
+var ucfirst = function ucfirst(str) {
+  str += String('');
+  var capital = str.charAt(0).toUpperCase();
+  return capital + str.substr(1);
+};
+
+var slugify = function slugify(input) {
+  return input.trim().replace(/\s+/g, '-').toLowerCase();
+};
+
+var namespacify = function namespacify(authorName) {
+  return authorName
+    .split(' ')
+    .map(function (part) {
+      return ucfirst(part);
+    })
+    .join('');
+};
 
 module.exports = yeoman.Base.extend({
   prompting: function () {
     this.log(yosay(
-      'Welcome to the fine ' + chalk.red('generator-php-package') + ' generator!'
+      'Welcome to the fine ' + chalk.red('PHP package') + ' generator!'
     ));
 
-    var slugify = function(input) {
-      return input.trim().replace(/\s+/g, '-').toLowerCase();
+    var required = function required(input) {
+      return input;
     };
 
-    var required = function(input) {
-      return input ? true : false;
-    };
-
-    var email = function(input) {
+    var email = function email(input) {
       return isEmailLike(input);
+    };
+
+    var url = function url(input) {
+      return /^(https?|ftp):\/\/[^\s/$.?#].[^\s]*$/i.test(input);
     };
 
     var prompts = [
       {
         type: 'input',
-        name: 'vendor',
+        name: 'packageName',
         message: 'Your package name?',
-        default: function() {
-          return slugify(path.basename(process.cwd()));
-        }
+        default: slugify(path.basename(process.cwd()))
       },
       {
         type: 'input',
-        name: 'vendor',
-        message: 'Your Composer vendor name?',
-        default: function() {
-          return username.sync();
-        },
+        name: 'packageDescription',
+        message: 'Your package description?',
         validate: required
       },
       {
         type: 'input',
-        name: 'author_name',
+        name: 'namespace',
+        message: 'Top level namespace?',
+        default: namespacify(this.user.git.name()),
+        validate: required
+      },
+      {
+        type: 'input',
+        name: 'authorName',
         message: 'Your full name?',
-        default: function() {
-          var done = this.async();
-
-          return fullname().then(function(name) {
-            done(null, name);
-          });
-        },
+        default: this.user.git.name(),
         validate: required
       },
       {
         type: 'input',
-        name: 'author_email',
+        name: 'authorEmail',
         message: 'Your e-mail address?',
+        default: this.user.git.email(),
         validate: email
       },
       {
         type: 'input',
-        name: 'author_username',
+        name: 'authorUsername',
         message: 'Your GitHub username?',
-        default: function() {
-          return username.sync();
-        },
+        default: username.sync(),
         validate: required
       },
       {
         type: 'input',
-        name: 'author_website',
+        name: 'authorWebsite',
         message: 'Your website?',
-        validate: required
+        validate: url
       }
     ];
 
@@ -84,14 +105,85 @@ module.exports = yeoman.Base.extend({
     }.bind(this));
   },
 
-  writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+  writing: function writing() {
+    var done = this.async();
+
+    var transformations = [
+      {
+        regexp: new RegExp('\\*\\*Note:.*\\n', 'g'),
+        replacement: ''
+      },
+      {
+        regexp: new RegExp('https://github.com/:vendor/:package_name', 'g'),
+        replacement: 'https://github.com/' + this.props.authorUsername + '/' + this.props.packageName
+      },
+      {
+        regexp: new RegExp(':vendor\\\\\\\\:package_name\\\\\\\\', 'g'),
+        replacement: this.props.namespace + '\\\\' + ucfirst(this.props.packageName)
+      },
+      {
+        regexp: new RegExp('League\\\\Skeleton;', 'g'),
+        replacement: this.props.namespace + '\\' + ucfirst(this.props.packageName) + '\\Skeleton;'
+      },
+      {
+        regexp: new RegExp(':vendor Test Suite', 'g'),
+        replacement: ucfirst(this.props.packageName) + ' Test Suite'
+      },
+      {
+        regexp: new RegExp(':author_name', 'g'),
+        replacement: this.props.authorName
+      },
+      {
+        regexp: new RegExp(':author_username', 'g'),
+        replacement: this.props.authorUsername
+      },
+      {
+        regexp: new RegExp(':author_website', 'g'),
+        replacement: this.props.authorWebsite
+      },
+      {
+        regexp: new RegExp(':author_email', 'g'),
+        replacement: this.props.authorEmail
+      },
+      {
+        regexp: new RegExp(':vendor', 'g'),
+        replacement: this.props.authorUsername
+      },
+      {
+        regexp: new RegExp(':package_name', 'g'),
+        replacement: this.props.packageName
+      },
+      {
+        regexp: new RegExp(':package_description', 'g'),
+        replacement: this.props.packageDescription
+      }
+    ];
+
+    var swapVariables = function swapVariables(contents) {
+      for (var i = 0; i < transformations.length; i++) {
+        var transformer = transformations[i];
+        contents = contents.replace(transformer.regexp, transformer.replacement);
+      }
+      return contents;
+    };
+
+    this.registerTransformStream(transform(swapVariables, {encoding: 'utf-8'}));
+    this.extract(skeletonTarballUri, '.', {}, function () {
+      var extractedDirectory = skeletonOrganisation + '-' + skeletonRepository + '-' + skeletonShortVersion;
+      this.directory(this.destinationPath(extractedDirectory), this.destinationRoot(), function () {});
+      done();
+    }.bind(this));
+
+    return done;
   },
 
-  install: function () {
-    this.installDependencies();
+  install: function install() {
+    var composerPath = shell.which('composer');
+    if (composerPath) {
+      this.log('Installing Composer dependencies...');
+      this.spawnCommandSync(composerPath, ['install']);
+    } else {
+      this.log.error('Composer not found. Please run `composer install` manually.');
+    }
   }
 });
--- test/app.js
+++ test/app.js
@@ -4,15 +4,28 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-php-package:app', function () {
+  this.timeout(20000);
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .withPrompts({
+        packageName: 'pipeline',
+        packageDescription: 'A fast PHP pipeline implementation',
+        namespace: 'HansOtt',
+        authorName: 'Hans Ott',
+        authorEmail: 'hansott@hotmail.be',
+        authorUsername: 'hansott',
+        authorWebsite: 'http://hansott.github.io/'
+      })
       .toPromise();
   });
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
-    ]);
+  it('creates files', function (done) {
+    var expectedContents = [
+      ['src/SkeletonClass.php', /namespace HansOtt\\Pipeline\\Skeleton;/],
+      ['README.md', /composer require hansott\/pipeline/],
+      ['composer.json', /"HansOtt\\\\Pipeline": "src"/]
+    ];
+    assert.fileContent(expectedContents);
+    done();
   });
 });
